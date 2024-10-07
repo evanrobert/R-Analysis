2ebe7bc552a80869056540e15359c181262f8146
evan <evan.lindsay223@gmail.com>
2024-10-07 17:30:46 -0400

    adding some mock data and analysis

---
+++ DataAnalysis.R
@@ -0,0 +1,47 @@
+library(dplyr)
+
+df <- read.csv("/Users/evanlindsay/Downloads/faker.csv")
+
+# 1
+for(i in 1:nrow(df)){
+  if(df$jobTitle[i] == "Regional Metrics Producer"){
+    print(df$jobArea[i])
+  }
+}
+
+# Alternative Option for # 1
+
+select(filter(df, jobTitle == 'Regional Metrics Producer'), jobArea)
+
+
+#2
+
+for(i in 1:nrow(df)){
+  if(df$firstName[i] == 'Daphnee'){
+    print(df$name[i])
+  }
+}
+# 2 Alternative
+select(filter(df, firstName == 'Daphnee'), name)
+
+#3
+for(i in 1:nrow(df)){
+  if(df$jobTitle[i] == "District Infrastructure Administrator"){
+    print(df$jobDescriptor[i] )
+  }
+}
+
+#4
+for(i in 1:nrow(df)){
+  if(df$gender[i]== 'Androgyne'){
+    print(df$firstName[i])
+    print(df$lastName[i])
+  }
+}
+
+# 4 Alternative
+
+select(filter(df, gender == 'Androgyne'),firstName)
+
+
+
